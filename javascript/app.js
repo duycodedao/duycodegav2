05ed1f3263bf1da22f2c85c63a6a174aa0f3cb07
duycodedao <111243534+duycodedao@users.noreply.github.com>
2022-08-24 12:11:13 +0700

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+      alert('Chào Mừng Bạn Đến Với Duy Code Dạo 2.0'); //Thông báo
+    // Tìm các li có sub-menu và thêm váo class has-child
+    $('.sub-menu').parent('li').addClass('has-child');
+
+});
---
+++ javascript/thathinh.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("mybtn");
+btn.addEventListener("click", anhYeuEm = () => {
+    for (let i = 0; i < 1; i++) {
+        document.getElementById("yeuEm").innerHTML += "<br/>😘 Này Cậu Cute Làm Người Yêu Tớ Nhé 😘 <br/>"
+    }
+})
